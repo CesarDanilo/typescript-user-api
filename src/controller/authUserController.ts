48f1568ba11d63ebec1e2cea48f4ac50ed5f8ff2
CSR <billypalacios616@gmail.com>
2025-07-24 14:31:11 -0300

    feat: authUser

--- src/controller/authUserController.ts
+++ src/controller/authUserController.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express'
+import validateDataEntry from '../services/validate-data-entry'
 
 export default async function authUser(req: Request, res: Response) {
-    const { email, senha } = req.body;
+    const { email, password } = req.body;
+    
+    if (await !validateDataEntry(email, password)) { console.log("Dados Invalidos!") };
+
 }
--- src/services/validate-data-entry.ts
+++ src/services/validate-data-entry.ts
@@ -1,8 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express'
 import { z } from 'zod';
 
-export default async function validateDataEntry(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
+export default async function validateDataEntry(email: String, password: String) {
 
     const schema = z.object({
         email: z.string().email('E-mail não é valido'),
@@ -14,12 +12,5 @@ export default async function validateDataEntry(req: Request, res: Response, nex
         password: password
     })
 
-    if (!result.success) {
-        return res.status(400).json({
-            msg: 'Informações inválidas',
-            errors: result.error
-        });
-    }
-
-    next();
+    return result.success
 }   
