050eec85bbd17bd2ec78dbcf4e285bf29d3e4df9
CSR <billypalacios616@gmail.com>
2025-07-24 14:48:40 -0300

    chore: validando campos

--- src/controller/authUserController.ts
+++ src/controller/authUserController.ts
@@ -3,7 +3,7 @@ import validateDataEntry from '../services/validate-data-entry'
 
 export default async function authUser(req: Request, res: Response) {
     const { email, password } = req.body;
-    
-    if (await !validateDataEntry(email, password)) { console.log("Dados Invalidos!") };
+
+    if (await validateDataEntry(email, password)) { console.log("Dados Invalidos!") };
 
 }
