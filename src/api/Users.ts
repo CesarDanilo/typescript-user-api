4b478fa9ccfe1e5ed4de9c0b30053be8e6f561a0
CSR <billypalacios616@gmail.com>
2025-07-25 14:57:07 -0300

    chore: create api

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import createUser from '../controller/createUserController'
 import UserDataValidation from '../middleware/UserDataValidation';
 import fetchUsers from '../controller/fetchUserController';
+import authUser from '../controller/authUserController';
 
 const route = express.Router();
 
 route.post('/create', UserDataValidation, createUser);
+route.post('/auth', authUser);
 route.get('/fetch', fetchUsers);
 
 export default route;
