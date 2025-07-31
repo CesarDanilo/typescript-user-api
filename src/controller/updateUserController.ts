87246621a63f4faf2b380a28f06b6203e832dbfc
CSR <billypalacios616@gmail.com>
2025-07-31 12:50:05 -0300

    feat: created new [updateUserController.ts]

---
+++ src/controller/updateUserController.ts
@@ -0,0 +1,6 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "../generated/prisma";
+
+export default async function updateUser(req: Request, res: Response) {
+  
+}
