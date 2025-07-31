f9e7dcc856a63202e7713fc2221aa4fd53027641
CSR <billypalacios616@gmail.com>
2025-07-31 12:06:59 -0300

    feat: created new controller [deleteUserController.ts]

---
+++ src/controller/deleteUserController.ts
@@ -0,0 +1,6 @@
+import express, { Request, Response } from 'express'
+import { PrismaClient } from "../generated/prisma";
+
+export default async function deleteUser(req: Request, res: Response) {
+
+}
