e93a194faf1f6d1c48a24385211140d58a4f26c8
CSR <billypalacios616@gmail.com>
2025-07-31 15:00:04 -0300

    feat: created new [updateUserController.ts]

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -4,6 +4,7 @@ import UserDataValidation from '../middleware/UserDataValidation';
 import fetchUsers from '../controller/fetchUserController';
 import authUser from '../controller/authUserController';
 import deleteUser from '../controller/deleteUserController';
+import updateUser from '../controller/updateUserController';
 
 const route = express.Router();
 
@@ -11,5 +12,6 @@ route.post('/create', UserDataValidation, createUser);
 route.post('/auth', authUser);
 route.get('/fetch', fetchUsers);
 route.delete('/destroy', deleteUser);
+route.put('/update', updateUser);
 
 export default route;
--- src/controller/updateUserController.ts
+++ src/controller/updateUserController.ts
@@ -2,5 +2,28 @@ import express, { Request, Response } from "express";
 import { PrismaClient } from "../generated/prisma";
 
 export default async function updateUser(req: Request, res: Response) {
-  
+  const prisma = new PrismaClient();
+  const { id } = req.query;
+
+  const data = req.body;
+
+  try {
+    if (!id) {
+      return res.status(400).json({
+        msg: "ID do usuário é obrigatório.",
+      });
+    }
+
+    const response = await prisma.user.update({ where: { id: String(id) }, data: data });
+
+    return res.status(200).json({
+      msg: `Usuário Atualizado com sucesso`,
+      user: response
+    });
+
+  } catch (erro) {
+    return res.status(500).json({
+      msg: `Algo deu errado: ${erro}`
+    });
+  }
 }
