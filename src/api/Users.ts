115b7501dbd4e94f6b76019de9510e2844d03afe
CSR <billypalacios616@gmail.com>
2025-07-31 12:46:30 -0300

    chore: finalizando [deleteUserController.ts]

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -3,11 +3,13 @@ import createUser from '../controller/createUserController'
 import UserDataValidation from '../middleware/UserDataValidation';
 import fetchUsers from '../controller/fetchUserController';
 import authUser from '../controller/authUserController';
+import deleteUser from '../controller/deleteUserController';
 
 const route = express.Router();
 
 route.post('/create', UserDataValidation, createUser);
 route.post('/auth', authUser);
 route.get('/fetch', fetchUsers);
+route.delete('/destroy', deleteUser);
 
 export default route;
--- src/controller/deleteUserController.ts
+++ src/controller/deleteUserController.ts
@@ -2,5 +2,16 @@ import express, { Request, Response } from 'express'
 import { PrismaClient } from "../generated/prisma";
 
 export default async function deleteUser(req: Request, res: Response) {
+  const prisma = new PrismaClient();
+  const { id } = req.query;
 
+  try {
+    const response = await prisma.user.delete({ where: { id: String(id) } });
+    return res.status(200).json({
+      msg: "Usuário deletado com súcesso"
+    })
+
+  } catch (erro) {
+    return res.status(500).json({ msg: `Algo deu errado: ${erro}` })
+  }
 }
