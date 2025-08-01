029d1a62faa85b84ce0d454457411cdccee184bd
CSR <billypalacios616@gmail.com>
2025-08-01 17:07:24 -0300

    chore: deploy v.06

--- src/controller/createUserController.ts
+++ src/controller/createUserController.ts
@@ -1,29 +1,29 @@
 import type { Request, Response } from 'express';
 import { PrismaClient } from '../generated/prisma';
 
-interface CreateUserBody {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-}
+// interface CreateUserBody {
+//     id: string;
+//     name: string;
+//     email: string;
+//     password: string;
+// }
 
 export default async function createUser(
-  req: Request<{}, {}, CreateUserBody>,
-  res: Response
+    req: Request<any, any, any, any>,
+    res: Response
 ) {
-  const data = req.body;
-  const prisma = new PrismaClient();
+    const data = req.body;
+    const prisma = new PrismaClient();
 
-  try {
-    const response = await prisma.user.create({ data });
-    return res.status(201).json({
-      msg: "Usuário criado com sucesso",
-      data: response,
-    });
-  } catch (erro) {
-    return res.status(500).json({
-      msg: `Erro ao criar usuário: ${erro}`,
-    });
-  }
+    try {
+        const response = await prisma.user.create({ data });
+        return res.status(201).json({
+            msg: "Usuário criado com sucesso",
+            data: response,
+        });
+    } catch (erro) {
+        return res.status(500).json({
+            msg: `Erro ao criar usuário: ${erro}`,
+        });
+    }
 }
