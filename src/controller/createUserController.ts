3287364ff67cc93166bbcb7a3abb3db47aa71515
CSR <billypalacios616@gmail.com>
2025-08-02 09:10:58 -0300

    deploy v.13

--- src/controller/createUserController.ts
+++ src/controller/createUserController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
 import { json } from 'micro';
 import { PrismaClient } from '../generated/prisma';
+import { IncomingMessage, ServerResponse } from 'http'; // Importe os tipos nativos
 
 interface CreateUserBody {
     id: string;
@@ -10,23 +10,26 @@ interface CreateUserBody {
 }
 
 export default async function createUser(
-    req: Request,
-    res: Response
+    req: IncomingMessage, // Use o tipo nativo
+    res: ServerResponse // Use o tipo nativo
 ) {
-    // Use a função json do 'micro' para obter o corpo da requisição
-    const data = await json(req) as CreateUserBody;
+    const data = await json(req) as CreateUserBody; // Agora o 'json' do 'micro' aceita 'req'
 
     const prisma = new PrismaClient();
 
     try {
         const response = await prisma.user.create({ data });
-        return res.status(201).json({
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
             msg: "Usuário criado com sucesso",
             data: response,
-        });
+        }));
     } catch (erro) {
-        return res.status(500).json({
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
             msg: `Erro ao criar usuário: ${erro}`,
-        });
+        }));
     }
 }
