e00c946c889bca32d218751f55e90ea6b25b2500
CSR <billypalacios616@gmail.com>
2025-08-02 08:54:43 -0300

    deploy v.11

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
     origin: '*',
--- src/controller/createUserController.ts
+++ src/controller/createUserController.ts
@@ -12,11 +12,20 @@ export default async function createUser(
     req: Request<{}, {}, CreateUserBody>,
     res: Response
 ) {
-    const data = req.body as CreateUserBody;
+    const { id, name, email, password } = req.body; // Desestruturar o corpo da requisição
+
+    // O id é gerado no backend, certo? Se não, a tipagem estaria incorreta, já que o usuário não pode enviar um id.
     const prisma = new PrismaClient();
 
     try {
-        const response = await prisma.user.create({ data });
+        const response = await prisma.user.create({
+            data: {
+                id,
+                name,
+                email,
+                password
+            }
+        });
         return res.status(201).json({
             msg: "Usuário criado com sucesso",
             data: response,
